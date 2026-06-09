49847ea4e74e47aae3f8067f7f45c915b12a25d1
Guilherme <guibecattini@gmail.com>
2026-06-09 15:35:06 -0300

    feat: estrutura inicial spring boot

---
+++ src/main/java/br/com/example/es2_saude/Es2SaudeApplication.java
@@ -0,0 +1,13 @@
+package br.com.example.es2_saude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Es2SaudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Es2SaudeApplication.class, args);
+	}
+
+}
